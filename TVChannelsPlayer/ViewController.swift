44c44beb14663a9f40b1ccd236161769ba80d0ef
TsaybelSA <tsaybelsa@gmail.com>
2023-06-10 08:36:56 +0700

    Initial Commit

---
+++ TVChannelsPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TVChannelsPlayer
+//
+//  Created by Сергей Цайбель on 10.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
